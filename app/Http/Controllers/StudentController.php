e95dc91bd986debe2767f5b83ba2c3e07b7245e7
unknown <manuel.eric.b@gmail.com>
2022-03-31 11:08:45 +0700

    revise search student function

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -7,6 +7,7 @@ use App\Providers\RouteServiceProvider;
 use Illuminate\Http\Request;
 use Exception;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\DB;
 
 class StudentController extends Controller
 {
@@ -24,7 +25,7 @@ class StudentController extends Controller
 
         try {
             $students = Students::where(function($query) use ($keyword) {
-                $query->where('first_name', 'like', '%'.$keyword.'%')->orWhere('last_name', 'like', '%'.$keyword.'%')->orWhere('email', 'like', '%'.$keyword.'%');
+                $query->where(DB::raw("CONCAT(`first_name`, ' ', `last_name`)"), 'like', '%'.$keyword.'%')->orWhere('email', 'like', '%'.$keyword.'%');
             })->paginate($this->ADMIN_LIST_STUDENT_VIEW_PER_PAGE);
         } catch (Exception $e) {
             Log::error('Find Student by Keyword Issue : ['.$keyword.'] '.$e->getMessage());
