b6b72c7abdb2cb787e40ef04cc77b73f1140a8c6
unknown <manuel.eric.b@gmail.com>
2022-04-13 11:23:36 +0700

    update index cors

--- public/index.php
+++ public/index.php
@@ -1,12 +1,7 @@
 <?php
 
-header("Access-Control-Allow-Origin: *");
-//header("Access-Control-Allow-Methods", "DELETE, POST, GET, OPTIONS");
-header("Access-Control-Allow-Headers:*");
-
-if ($_SERVER['REQUEST_METHOD'] == "OPTIONS") {//send back preflight request response
-return "";
-}
+header('Access-Control-Allow-Origin: *');  
+header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
 
 use Illuminate\Contracts\Http\Kernel;
 use Illuminate\Http\Request;
