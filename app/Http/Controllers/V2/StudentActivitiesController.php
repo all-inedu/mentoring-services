16a87712d861b54b30e7723d7652348579aa80d8
unknown <manuel.eric.b@gmail.com>
2022-06-24 09:40:52 +0700

    revise list meeting

--- app/Http/Controllers/V2/StudentActivitiesController.php
+++ app/Http/Controllers/V2/StudentActivitiesController.php
@@ -39,45 +39,76 @@ class StudentActivitiesController extends Controller
         $use_keyword = $request->get('keyword') ? 1 : 0;
         $keyword = $request->get('keyword') != NULL ? $request->get('keyword') : null;
 
+        // $activities = StudentActivities::with('students', 'users')->whereHas('programmes', function($query) use ($programme) {
+        //         $query->where('prog_name', $programme);
+        // })->when($use_keyword, function($query) use ($keyword, $programme, $status) {
+        //     $query->when($programme == "1-on-1-call", function ($q1) use ($keyword, $status) {
+        //         $q1->where(function($q2) use ($keyword) {
+        //             $q2->where(DB::raw("CONCAT(`module`, ' - ', `call_with`)"), 'like', '%'.$keyword.'%')->orWhereHas('users', function($q3) use ($keyword) {
+        //                 $q3->where(DB::raw("CONCAT(`first_name`, ' ', `last_name`)"), 'like', '%'.$keyword.'%');
+        //             })->orWhereHas('students', function ($q) use ($keyword) {
+        //                 $q->where(DB::raw("CONCAT(`first_name`, ' - ', `last_name`)"), 'like', '%'.$keyword.'%');
+        //             });
+        //         })->when($status == 'new', function ($q) {
+        //             $q->where('std_act_status', 'waiting')->where('mt_confirm_status', 'confirmed')->where('call_status', 'waiting');
+        //         })->when($status == 'pending', function ($q) {
+        //             $q->where('std_act_status', 'confirmed')->where('mt_confirm_status', 'waiting')->where('call_status', 'waiting');
+        //         })->when($status == 'upcoming', function ($q) {
+        //             $q->where('std_act_status', 'confirmed')->where('mt_confirm_status', 'confirmed')->where('call_status', 'waiting');
+        //         })->when($status == "history", function ($q) {
+        //             $q->where(function ($q1) { // history dari call status yg berhasil
+        //                 $q1->where('std_act_status', 'confirmed')->where('mt_confirm_status', 'confirmed')->where('call_status', 'finished');
+        //             })->orWhere(function ($q1) { // history dari call status yg cancel 
+        //                 $q1->where('std_act_status', 'cancel')->where('mt_confirm_status', 'confirmed')->where('call_status', 'canceled');
+        //             })->orWhere(function ($q1) {
+        //                 $q1->where('std_act_status', 'confirmed')->where('mt_confirm_status', 'cancel')->where('call_status', 'canceled');
+        //             });
+        //         });
+        //     });
+        // })->whereHas('students', function ($q) {
+        //     $q->where('id', $this->student_id);
+        // })->orderBy('call_status', 'desc')
+        // ->orderBy('call_date', 'asc')->recent($recent, $this->STUDENT_MEETING_VIEW_PER_PAGE);
+
         $activities = StudentActivities::with('students', 'users')->whereHas('programmes', function($query) use ($programme) {
                 $query->where('prog_name', $programme);
-        })->when($use_keyword, function($query) use ($keyword, $programme, $status) {
-            $query->when($programme == "1-on-1-call", function ($q1) use ($keyword, $status) {
+        })->when($use_keyword, function($query) use ($keyword, $programme) {
+            $query->when($programme == "1-on-1-call", function ($q1) use ($keyword) {
                 $q1->where(function($q2) use ($keyword) {
                     $q2->where(DB::raw("CONCAT(`module`, ' - ', `call_with`)"), 'like', '%'.$keyword.'%')->orWhereHas('users', function($q3) use ($keyword) {
                         $q3->where(DB::raw("CONCAT(`first_name`, ' ', `last_name`)"), 'like', '%'.$keyword.'%');
                     })->orWhereHas('students', function ($q) use ($keyword) {
                         $q->where(DB::raw("CONCAT(`first_name`, ' - ', `last_name`)"), 'like', '%'.$keyword.'%');
                     });
-                })->when($status == 'new', function ($q) {
-                    $q->where('std_act_status', 'waiting')->where('mt_confirm_status', 'confirmed')->where('call_status', 'waiting')
-                    ->orderBy('call_status', 'desc')
-                    ->orderBy('call_date', 'asc');
-                })->when($status == 'pending', function ($q) {
-                    $q->where('std_act_status', 'confirmed')->where('mt_confirm_status', 'waiting')->where('call_status', 'waiting')
-                    ->orderBy('call_status', 'desc')
-                    ->orderBy('call_date', 'asc');
-                })->when($status == 'upcoming', function ($q) {
-                    $q->where('std_act_status', 'confirmed')->where('mt_confirm_status', 'confirmed')->where('call_status', 'waiting')
-                    ->orderBy('call_status', 'desc')
-                    ->orderBy('call_date', 'asc');
-                })->when($status == "history", function ($q) {
-                    $q->where(function ($q1) { // history dari call status yg berhasil
-                        $q1->where('std_act_status', 'confirmed')->where('mt_confirm_status', 'confirmed')->where('call_status', 'finished');
-                    })->orWhere(function ($q1) { // history dari call status yg cancel 
-                        $q1->where('std_act_status', 'cancel')->where('mt_confirm_status', 'confirmed')->where('call_status', 'canceled');
-                    })->orWhere(function ($q1) {
-                        $q1->where('std_act_status', 'confirmed')->where('mt_confirm_status', 'cancel')->where('call_status', 'canceled');
-                    })->orderBy('call_status', 'desc')->orderBy('created_at', 'desc');
                 });
             });
         })->whereHas('students', function ($q) {
             $q->where('id', $this->student_id);
-        })
-        // ->when($using_status, function($query) use ($status){
-        //     $query->where('std_act_status', $status);
-        // })
-        ->recent($recent, $this->STUDENT_MEETING_VIEW_PER_PAGE);
+        })->when($status == 'new', function ($q) {
+            $q->where('std_act_status', 'waiting')->where('mt_confirm_status', 'confirmed')->where('call_status', 'waiting')
+            ->orderBy('call_status', 'desc')
+            ->orderBy('call_date', 'asc');
+        })->when($status == 'pending', function ($q) {
+            $q->where('std_act_status', 'confirmed')->where('mt_confirm_status', 'waiting')->where('call_status', 'waiting')
+            ->orderBy('call_status', 'desc')
+            ->orderBy('call_date', 'asc');
+        })->when($status == 'upcoming', function ($q) {
+            $q->where('std_act_status', 'confirmed')->where('mt_confirm_status', 'confirmed')->where('call_status', 'waiting')
+            ->orderBy('call_status', 'desc')
+            ->orderBy('call_date', 'asc');
+        })->when($status == "history", function ($q) {
+            $q->where(function ($q1) { // history dari call status yg berhasil
+                $q1->where('std_act_status', 'confirmed')->where('mt_confirm_status', 'confirmed')->where('call_status', 'finished');
+            })->orWhere(function ($q1) { // history dari call status yg cancel 
+                $q1->where('std_act_status', 'cancel')->where('mt_confirm_status', 'confirmed')->where('call_status', 'canceled');
+            })->orWhere(function ($q1) {
+                $q1->where('std_act_status', 'confirmed')->where('mt_confirm_status', 'cancel')->where('call_status', 'canceled');
+            })
+            ->orderBy('call_status', 'desc')
+            ->orderBy('call_date', 'desc');
+        })->whereHas('students', function ($q) {
+            $q->where('id', $this->student_id);
+        })->recent($recent, $this->STUDENT_MEETING_VIEW_PER_PAGE);
 
         return response()->json(['success' => true, 'data' => $activities]);
     }
