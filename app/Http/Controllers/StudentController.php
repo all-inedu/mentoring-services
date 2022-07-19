77c130fba0dc990e2f1c13045e94f84dbe0d3549
unknown <manuel.eric.b@gmail.com>
2022-07-19 14:39:29 +0700

    update student detail func

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -135,9 +135,12 @@ class StudentController extends Controller
     
     public function index($student_id = NULL, Request $request)
     {
-        $is_detail = $student_id != NULL ? 1 : 0;
+        $is_detail = (($student_id != NULL) || ($request->get('mail') != NULL)) ? 1 : 0;
+        $email = $request->get('mail') != NULL ? $request->get('mail') : null;
         $students = Students::with('social_media')->orderBy('created_at', 'desc')->when($student_id != NULL, function($query) use ($student_id) {
             $query->where('id', $student_id);
+        })->when($email != NULL, function($query) use ($email) {
+                $query->where('email', $email);
         })->paginateChecker($is_detail, $this->ADMIN_LIST_STUDENT_VIEW_PER_PAGE);
         return response()->json(['success' => true, 'data' => $students]);
 
