3ba7df62dce65bb43ef5ae675d0fa7eb28d69c1f
unknown <manuel.eric.b@gmail.com>
2022-08-04 13:53:40 +0700

    ++ group project all admin

--- app/Http/Controllers/GroupProjectController.php
+++ app/Http/Controllers/GroupProjectController.php
@@ -10,7 +10,7 @@ class GroupProjectController extends Controller
     public function index()
     {
         $group_project = GroupProject::select(['id', 'project_name', 'project_type', 'project_desc', 'progress_status', 'status'])->
-                    with(['group_participant:id,first_name,last_name', 'assigned_mentor:id,first_name,last_name'])->get();
+                    withCount(['group_participant as total_member', 'assigned_mentor as total_mentor'])->get();
         return response()->json(['success' => true, 'data' => $group_project]);
     }
 }
